58f7cf23bde5e864a5e92a5a550bbd5dbb1c1eca
tingzhang <zt.freedom@gmail.com>
2017-01-11 10:23:47 +0800

    优惠券BUG处理

--- http_handler/login.py
+++ http_handler/login.py
@@ -75,6 +75,8 @@ class Login:
                 self.uuid, sign[1], sign[0]),
             data='body=' + urllib.quote(json.dumps(body)) + '&',
             headers=headers)
+        logger.debug(resp.url)
+        logger.debug(resp.text)
         list = resp.json()
         if id:
             return filter(lambda c: c['id'] == id,list['couponList'])
